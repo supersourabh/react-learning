d6db78c0dd9c22330870a48c0754ea3cbf7041bb
supersourabh <kotagisourabh81@gmail.com>
2025-03-16 15:27:46 +0530

    useActionState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useCallback, useMemo, useState, useDeferredValue, Suspense, useEffect, useId, useReducer, useRef, Profiler } from 'react'
+import { useCallback, useMemo, useState, useDeferredValue, Suspense, useEffect, useId, useReducer, useRef, Profiler, useActionState } from 'react'
 import './App.css'
 import { ThemeContext } from './contexts'
 import Test from './test';
 
 
-
-
 function App() {
   console.log('rendered');
   
-  const iref = useRef(null)
 
+  function increment(prevState, formData){
+    console.log(formData.get("name"));
+    return prevState + 1;
+  }
 
+  const [ number ,action , isPending] = useActionState(increment, 0);
   return (
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <Test />
-    </Suspense>
+    <form>
+      {isPending ? "Loading...": number} <br />
+      <input  type="text" name='name' value="Sourabh" /> <br />
+      <button formAction={action}>
+        Click me
+      </button>
+    </form>
   );
 }
+
 export default App
 
 const Loading = () => {
