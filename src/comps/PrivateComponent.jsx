69e2f94da650fc582d92f04bf169ffb81854634e
supersourabh <kotagisourabh81@gmail.com>
2025-03-16 20:08:56 +0530

    react router dom - private route fixed the bug

--- src/comps/PrivateComponent.jsx
+++ src/comps/PrivateComponent.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Navigate, Route } from "react-router-dom";
 
-export default function ({ childrens }) {
-  const userInfo = { id: 1, name: "sk" };
-  alert("unauthorized redirecting to Home page");
-  return userInfo && userInfo.token ? { childrens } : <Navigate to="/" />;
+export default function ({children}) {
+  const userInfo = { id: 1, name: "sk", token: "null" };
+  console.log(children);
+  
+  // alert("unauthorized redirecting to Home page");
+  return userInfo && userInfo.token ? children : <Navigate to="/" />;
 }
