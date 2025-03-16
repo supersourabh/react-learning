df3bd52689177b20ee9f9af0f0083b798d337ea5
supersourabh <kotagisourabh81@gmail.com>
2025-03-16 14:54:52 +0530

    use and suspense

--- src/test.tsx
+++ src/test.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from "react";
 
 export default function Test() {
-    const [data, setData] = useState()
+  const [res, setRes] = useState(false)
+  const data = use(
+    new Promise((resolve, reject) => {
+      let num = Math.floor(Math.random() * 100);
+      num > 50 ? resolve("resolved") : reject("rejected");
+    })
+      .then(() => "resolved")
+      .catch(() => "rejected")
+  );
 
-    useEffect(() => {
-         gd()
-    }, [])
-    
-    
-    return (
-        <div>
-            Hello world { data }       
-        </div>
-    )
-}
-async function gd(){
-    return await fetch("https://jsonplaceholder.typicode.com/todos/1").then(res=>res.json())
+  return (
+    <div>
+      <h1>
+        Hello : <span style={{ color: data === 'resolved'?"green":"red" }}>Promise {data} </span>
+      </h1>
+    </div>
+  );
 }
